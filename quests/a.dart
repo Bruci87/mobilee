5c0295e0a38c200dba07a68ca8f1d69a20028efc
Silvio Bruciaferro <116919319+Bruci87@users.noreply.github.com>
2026-08-13 10:20:46 -0300

    Add files via upload

---
+++ quests/a.dart
@@ -0,0 +1,9 @@
+void main() {
+  int b = 3;
+  var nome = "jose";
+  print("Bom dia!: ");
+  print("Ola" + "Mundo");
+  print("$nome Aula");
+  print("${nome.length} caracteres");
+  print("$b Hello World");
+}
